f0231ba7abc7701345d06a2d8b3373edae596f0e
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-06-06 17:44:34 -0400

    finished lab

--- exercise-6.py
+++ exercise-6.py
@@ -1,21 +1,30 @@
 # exercise-06 What's the  Season?
 
-# Write the code that:
-# 1. Prompts the user to enter the month (as three characters):
-#      Enter the month of the year (Jan - Dec):
-# 2. Then prompts the user to enter the day of the month: 
-#      Enter the day of the month:
-# 3. Calculate what season it is based upon this chart:
-#      Dec 21 - Mar 19: Winter
-#      Mar 20 - Jun 20: Spring
-#      Jun 21 - Sep 21: Summer
-#      Sep 22 - Dec 20: Fall
-# 4. Print the result as follows:
-#      <Mmm> <dd> is in <season> 
 
-# Hints:
-# Consider using the in operator to check if a string is in a particular list/tuple like this:
-# if input_month in ('Jan', 'Feb', 'Mar'):
-#
-# After setting the likely season, you can use another if...elif...else statement to "adjust" if
-# the day number falls within a certain range.
+input_month = input("Enter the month of the year (Jan - Dec):")
+month= input_month.lower()
+day = int(input("Enter the day of the month:"))
+
+if month in ('jan','feb'):
+    print(f'{input_month} {day} is in Winter')
+if (month == 'dec' and day > 20) or (month == 'mar' and day < 20):
+    print(f'{input_month} {day} is in Winter')
+
+if month in ('apr','may'):
+    print(f'{input_month} {day} is in Spring')
+if (month == 'mar' and day > 19) or (month == 'jun' and day < 21):
+    print(f'{input_month} {day} is in Spring')
+
+if month in ('jul','aug'):
+    print(f'{input_month} {day} is in Summer')
+if (month == 'jun' and day > 20) or (month == 'sep' and day < 22):
+    print(f'{input_month} {day} is in Summer')
+
+if month in ('oct','nov'):
+    print(f'{input_month} {day} is in Fall')
+if (month == 'sep' and day > 21) or (month == 'dec' and day < 21):
+    print(f'{input_month} {day} is in Fall')
+
+
+
+
